b6c109ff01e6b33f98f738266f9385adc7894fb6
Magiz0r <fomag924@gmail.com>
2026-04-13 15:35:52 -0700

    HW1

--- HW1/cube_demo.m
+++ HW1/cube_demo.m
@@ -9,8 +9,13 @@ body1.Joint = joint1;
 addVisual(body1, 'box', [0.25 0.25 0.25]);
 addBody(cube,body1,cube.BaseName);
 
-% Use this to change example output
-whichExample = 7;
+pos_init = [0 0 0]';
+orient_init = so3(eye(3));
+q_init = [quat(orient_init)'; pos_init];
+
+% Use this to change example output:
+% 1-5 is for Part A, 6-7 for Part B, 8 for Part C
+whichExample = 5;
 
 
 %------------------------------------------------------------------------------
@@ -49,7 +54,7 @@ elseif whichExample == 2
     'JointPosition: ', sprintf('%.2f ', q')];
 
 % ----------------------------------------------------------------------------
-% a. Generate figures for the following orientations
+% Part A. Generate figures for the following orientations
 elseif whichExample == 3
 
   % 45 degree for Z axis
@@ -103,17 +108,17 @@ elseif whichExample == 5
     'Example 5: Cube rotated by 45 deg about x-space axis\n', ...
     'JointPosition: ', sprintf('%.2f ', q')];
 
-% end of a.
+% end of Part A
 % ----------------------------------------------------------------------------
 
 
 % ----------------------------------------------------------------------------
-% b. Successive Rotations
+% Part b. Successive Rotations
 
+% part_b_1
 elseif whichExample == 6
 
-  % Successive rotations for x->y->z with 45 degree for each one
-  % succ- Successive Rotation Matrix using anonymous function
+  % Space frame, successive rotations for z<-y<-x with 45 degree for each one
   Rx =@(a) [1 0 0;
             0 cos(a) -sin(a);
             0 sin(a) cos(a)];
@@ -130,17 +135,25 @@ elseif whichExample == 6
 
   phiz = pi/4;
   position = [0 0 0]';
-  orientation = so3(Rsucc(phiz));
-  q = [quat(orientation)'; position];
 
-  note = [ ...
-    'Example 6: Cube Successively rotated by 45 degree about x, y, z axis \n', ...
-    'JointPosition: ', sprintf('%.2f ', q')];
+  orientation_rx = so3(Rx(phiz));
+  qx = [quat(orientation_rx)'; position];
 
+  orientation_ryrx = so3(Ry(phiz) * Rx(phiz));
+  qyqx = [quat(orientation_ryrx)'; position];
+
+  orientation_rzryrx = so3(Rz(phiz)* Ry(phiz) * Rx(phiz));
+  qzqyqx = [quat(orientation_rzryrx)'; position];
+  % q = [quat(orientation)'; position];
+
+  % note = [ ...
+  %   'Example 6: Cube Successively rotated by 45 degree about x, y, z axis \n', ...
+  %   'JointPosition: ', sprintf('%.2f ', q')];
+
+
+% part_b_2
 elseif whichExample == 7
-
-  % Successive rotations for z->y->x with 45 degree for each one
-  % succ- Successive Rotation Matrix using anonymous function
+  % Body frame, Successive rotations for z->y->x with 45 degree for each one
   Rx =@(a) [1 0 0;
             0 cos(a) -sin(a);
             0 sin(a) cos(a)];
@@ -153,28 +166,185 @@ elseif whichExample == 7
             sin(a) cos(a) 0;
             0 0 1];
 
-  Rsucc =@(a) Rx(a) * Ry(a) * Rz(a);
+  Rsucc =@(a) Rz(a) * Ry(a) * Rx(a);
 
   phiz = pi/4;
   position = [0 0 0]';
-  orientation = so3(Rsucc(phiz));
-  q = [quat(orientation)'; position];
+  orientation_rz = so3(Rz(phiz));
+  qz = [quat(orientation_rz)'; position];
 
-  note = [ ...
-    'Example 7: Cube Successively rotated by 45 degree about z, y, x axis \n', ...
-    'JointPosition: ', sprintf('%.2f ', q')];
+  orientation_rzry = so3(Rz(phiz) * Ry(phiz));
+  qzqy = [quat(orientation_rzry)'; position];
+
+  orientation_rzryrx = so3(Rz(phiz)* Ry(phiz) * Rx(phiz));
+  qzqyqx = [quat(orientation_rzryrx)'; position];
+  % q = [quat(orientation)'; position];
+  % 
+  % note = [ ...
+  %   'Example 7: Cube Successively rotated by 45 degree about z, y, x axis \n', ...
+  %   'JointPosition: ', sprintf('%.2f ', q')];
 
 % end of b.
 % ----------------------------------------------------------------------------
 
-
-
 end
 
 
+
+
+
+%----------------------------------------------------------------------------
+% part b plotting
+% b.1 (To see the right plot, please change whichExample to 6)
+if whichExample == 6
+    figure;
+    
+    subplot(1, 4, 1)
+    show(cube, q_init);
+    title("Initial Orientation");
+    hold on;
+    plotTransforms(pos_init', orient_init, 'FrameSize', 0.3);
+    axis_style();
+    
+    subplot(1, 4, 2);
+    show(cube, qx);
+    hold on;
+    title("Rx(45 degree)");
+    plotTransforms([0 0 0], so3(eye(3)), 'FrameSize',0.5)
+    plotTransforms(pos_init', orientation_rx, 'FrameSize', 0.3, 'FrameColor', [0, 0, 0]);
+    axis_style();
+    
+    subplot(1, 4, 3);
+    show(cube, qyqx);
+    hold on;
+    title("Ry(45 degree)Rx(45 degree)");
+    plotTransforms([0 0 0], so3(eye(3)), 'FrameSize',0.5)
+    plotTransforms(pos_init', orientation_ryrx, 'FrameSize', 0.3, 'FrameColor', [0, 0, 0]);
+    axis_style();
+    
+    subplot(1, 4, 4);
+    show(cube, qzqyqx);
+    hold on;
+    title("Rz(45 degree)Ry(45 degree)Rx(45 degree)");
+    plotTransforms([0 0 0], so3(eye(3)), 'FrameSize',0.5)
+    plotTransforms(pos_init', orientation_rzryrx, 'FrameSize', 0.3,'FrameColor', [0, 0, 0]);
+    axis_style();
+
+    % saveas(gcf, "PartB_1.jpg");
+end
+
+% b.2 (To see the right plot, please change whichExample to 7)
+if whichExample == 7
+    figure;
+    
+    subplot(1, 4, 1)
+    show(cube, q_init);
+    title("Initial Orientation");
+    hold on;
+    plotTransforms(pos_init', orient_init, 'FrameSize', 0.3);
+    axis_style();
+    
+    subplot(1, 4, 2);
+    show(cube, qz);
+    hold on;
+    title("Rz(45 degree)");
+    plotTransforms([0 0 0], so3(eye(3)), 'FrameSize',0.5)
+    plotTransforms(pos_init', orientation_rz, 'FrameSize', 0.3, 'FrameColor', [0, 0, 0]);
+    axis_style();
+    
+    subplot(1, 4, 3);
+    show(cube, qzqy);
+    hold on;
+    title("Rz(45 degree)Ry(45 degree)");
+    plotTransforms([0 0 0], so3(eye(3)), 'FrameSize',0.5)
+    plotTransforms(pos_init', orientation_rzry, 'FrameSize', 0.3, 'FrameColor', [0, 0, 0]);
+    axis_style();
+    
+    subplot(1, 4, 4);
+    show(cube, qzqyqx);
+    hold on;
+    title("Rz(45 degree)Ry(45 degree)Rx(45 degree)");
+    plotTransforms([0 0 0], so3(eye(3)), 'FrameSize',0.5)
+    plotTransforms(pos_init', orientation_rzryrx, 'FrameSize', 0.3,'FrameColor', [0, 0, 0]);
+    axis_style();
+
+    % saveas(gcf, "PartB_2.jpg");
+end
+%----------------------------------------------------------------------------
+
+
+
+%----------------------------------------------------------------------------
+% part c
+
+if whichExample == 8
+    v = VideoWriter('animation.avi', 'Motion JPEG AVI');
+    open(v);
+    
+    numOfFrames = 120;
+    p0 = [0 0.25 0.25]';
+    Ry =@(a) [cos(a) 0 sin(a);
+            0 1 0;
+            -sin(a) 0 cos(a)];
+    Rz =@(a) [cos(a) -sin(a) 0;
+            sin(a) cos(a) 0;
+            0 0 1];
+    
+    % angular velocity for body frame
+    w1 = 0.05;
+    
+    % angular velocity for rotating around space frame
+    w2 = 0.05;
+    
+    
+    figure;
+    for i = 1:numOfFrames
+        gcf; clf;
+    
+        theta1 = w1 * i;
+        theta2 = w2 * i;
+        
+        % rotate 45 about y_body then continus rotating about z_body
+        R_body = Ry(pi/4) * Rz(theta1);
+    
+        % rotating around z_space
+        p = Rz(theta2) * p0;
+        
+        % quat config for cube self-rotation
+        q = [quat(so3(R_body))'; p];
+    
+        % quat config for cube rotating around z_space
+        q_rot = quat(so3(R_body));
+       
+        show(cube, q);
+        hold on;
+        
+        plotTransforms([0 0 0], so3(eye(3)), 'Framesize', 0.5);
+        plotTransforms(p', q_rot, 'Framesize', 0.5);
+        axis_style()
+    
+        frame = getframe(gcf);
+        writeVideo(v, frame);
+        pause(0.01);
+    end
+    close(v);
+end
+% end of part c
+%----------------------------------------------------------------------------
+
+
+
+
+
+%----------------------------------------------------------------------------
+% code from the oringial cube_code.m
+% only for whichExample = 1-5
+% Please manually change the title/filename
 figure;
   % Display cube at current configuration
   show(cube,q); hold all;
+
+  title("45 rotation about x_s axis");
 
   % Visualize space frame
   plotTransforms([0 0 0], so3(eye(3)), 'FrameSize',0.5)
@@ -190,6 +360,7 @@ figure;
   ah.EdgeColor = 'none';
   ah.FontSize = 14;
 
+  saveas(gcf, "45_xAxis.jpg");
 % -----------------------------------------------------------------------------
 
 function axis_style()
